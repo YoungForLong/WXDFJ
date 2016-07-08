9aa4f3ddcf0d0b512c45f7380ec095d59e020419
Dophin <940101379@qq.com>
2016-07-08 17:33:32 +0800

    startScene

--- src/app/scenes/StartScene.lua
+++ src/app/scenes/StartScene.lua
@@ -5,16 +5,52 @@ local StartScene=class("StartScene",function()
 	end)
 
 function StartScene:ctor()
-	
+	self:init()
+	self:setStyle()
 end
 
-function init()
-	if(self!=nil) then
+function StartScene:init()
+	
+	if self then
 		-- 加入缓存
-		local SpriteCache=cc.cc.SpriteFrameCache:getInstance()
-
-		SpriteCache:
 		
+		-- img
+		display.addSpriteFrames("shoot.plist","shoot.png")
+		display.addSpriteFrames("shoot_background.plist","shoot_background.png")
+
+		-- audio
+		audio.preloadMusic("sound/bgmusic.mp3")
+		audio.preloadMusic("sound/hero_down.mp3")
+		audio.preloadMusic("sound/enermy1_down.wav")
+		audio.preloadMusic("sound/enermy2_down.wav")
+		audio.preloadMusic("sound/enermy3_down.wav")
+		audio.preloadMusic("sound/shoot.wav")
+
+		-- data
+		if cc.UserDefault:getInstance() then
+			cc.UserDefault:getInstance():setStringForKey("initTag", "initialized")
+			cc.UserDefault:getInstance():flush()
+			print("RunTime : Data Initialized")
+		end 
+
 	end
 end
+
+function StartScene:setStyle()
+	
+	-- bg
+
+	local bgSp=cc.Sprite:createWithSpriteFrameName("background.png")
+	bgSp:setAnchorPoint(0.5,0)
+	bgSp:setPosition(display.cx,0)
+	self:addChild(bgSp)
+
+	-- item
+
+	local startBtn=cc.cc.Sprite:createWithSpriteFrameName("game_resume_nor.png")
+	self:addChild(startBtn,10)
+
+end
+
+
 return StartScene
