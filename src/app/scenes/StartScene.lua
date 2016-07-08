d4765238625f53ebb89e18d11112cb594836e524
Dophin <940101379@qq.com>
2016-07-08 12:02:28 +0800

    res

--- src/app/scenes/StartScene.lua
+++ src/app/scenes/StartScene.lua
@@ -1,1 +1,20 @@
 -- StartScene
+
+local StartScene=class("StartScene",function()
+	return display.newScene("StartScene")
+	end)
+
+function StartScene:ctor()
+	
+end
+
+function init()
+	if(self!=nil) then
+		-- 加入缓存
+		local SpriteCache=cc.cc.SpriteFrameCache:getInstance()
+
+		SpriteCache:
+		
+	end
+end
+return StartScene
