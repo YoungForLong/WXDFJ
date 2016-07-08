35956a3a2531a1468b422d631fb1ba4e6658b590
Clark Young <puredesinger@outlook.com>
2016-07-08 18:10:19 +0800

    getStartScene

--- src/app/scenes/StartScene.lua
+++ src/app/scenes/StartScene.lua
@@ -5,16 +5,18 @@ local StartScene=class("StartScene",function()
 	end)
 
 function StartScene:ctor()
-	
+	self:init()
 end
 
-function init()
-	if(self!=nil) then
-		-- 加入缓存
-		local SpriteCache=cc.cc.SpriteFrameCache:getInstance()
-
-		SpriteCache:
-		
+function StartScene:init()
+	if(self == nil) then
+		exit()
 	end
+		-- 加入缓存
+	display.addSpriteFrames("shoot.plist","shoot.png")
+	display.addSpriteFrames("shoot_background.plist","shoot_background.png")
+
+	local sharedSpriteFrameCache=cc.SpriteFrameCache:getInstance()
+	print(sharedSpriteFrameCache)
 end
 return StartScene
